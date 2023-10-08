42debc88d6b7c4e6db83a353ca439d759bc7b933
Pranav Raju <120582474+pranavraju1@users.noreply.github.com>
2023-10-08 13:39:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let a = document.createElement("h1");
+let container = document.getElementById("container");
+container.appendChild(a);
+function updateSize(){
+    a.innerText="width: "+`${window.outerWidth}`+" and Height: "+`${window.outerWidth}`;
+}
+window.addEventListener('resize',updateSize);
+
